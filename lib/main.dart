41795a1a32caf451b20c174be8b16223e258c389
Asri695 <2006111@itg.ac.id>
2023-06-24 12:22:56 +0700

    Al-quran

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ahlul_quran_app/bloc/ayat/ayat_bloc.dart';
 import 'package:flutter_ahlul_quran_app/cubit/ayat/ayat_cubit.dart';
